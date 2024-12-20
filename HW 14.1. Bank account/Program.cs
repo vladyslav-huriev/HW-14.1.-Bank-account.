0f85c76b1e7a68fe62ee68284b86769a403351cd
Vladyslav <vladyslav.guriev@gmail.com>
2024-12-20 15:45:19 +0100

    Add project files.

---
+++ HW 14.1. Bank account/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Account
+{
+    public string Name {get;}
+    private double _balance;
+    public double Balance => _balance;
+    public Account(string name, double initialBalance)
+    {
+        Name = name;
+        _balance = initialBalance < 0 ? 0 : initialBalance;
+    }
+    public void Deposit(double amount) => _balance += amount > 0 ? amount : 0;
+    public void Withdrawal(double amount)
+        => _balance -= amount > 0 && amount <= _balance ? amount : 0;
+}
+class Program
+{
+    static void Main()
+    {
+        Account vladAccount = new Account("Vlad's account", 100);
+        Account vladPlAccount = new Account("Vlad's account in Poland", 1000000);
+        vladAccount.Withdrawal(20);
+        Console.WriteLine($"The balance of Vlad's account is now: {vladAccount.Balance}");
+        vladPlAccount.Deposit(200);
+        Console.WriteLine($"The balance of Vlad's other account is now: {vladPlAccount.Balance}");
+    }
+}
